9b3f8786d972c10ad2a3352666ba35c66d590296
Vincent L'honoré <vincent.lhonore@gmail.com>
2013-11-29 14:13:09 +0100

    solving the solving

--- routes/index.js
+++ routes/index.js
@@ -4,14 +4,12 @@
  */
 
 exports.index = function(req, res){
-<<<<<<< HEAD
+
 	res.render('index', { title: 'Locate It', login: req.session.login });
 };
 
 exports.api = function(req, res){
 	res.render('api', {title : 'Locate It API'});
-}
-=======
-  res.render('index', { title: 'Locate It', login: req.session.login });
 };
->>>>>>> branch 'master' of https://github.com/letcheck/locateit.git
+
+
