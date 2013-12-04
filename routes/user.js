900cfbb426003ef0149c4f71e3d9505e05154509
Vincent L'honoré <vincent.lhonore@gmail.com>
2013-12-04 14:58:57 +0100

    the userid session

--- routes/user.js
+++ routes/user.js
@@ -8,7 +8,6 @@ var sys = require('util'),
 
 var serverAddress = "http://127.0.0.1:5000";
 var serverPort = "5000";
-var idd = "";
 
 exports.list = function(req, res){
   res.send("respond with a resource");
@@ -21,20 +20,23 @@ exports.oauth2callback = function(req, res){
 		gapi.plus.people.get({ userId: 'me'}).withAuthClient(gapi.client).execute(function(err, results){
 			req.session.login = true;
 			req.session.name = results.displayName;
-			var id = store(req, res);console.log(id);
-			req.session.userid = ""+id;
-			
-			res.redirect('/');
+			store(req, res);			
 		});
 	});
 };
 
 function store (req, res){
-	
+	var idd = "";
 	rest.post(serverAddress+'/users', {
 		  data: { name:req.session.name/*, googleid:req.session.googleid, email: req.session.email*/},
-		}).on('complete', handleReturnServer);
-	req.session.userid = idd;
+		}).on('complete', function (data, response) {
+			  if (response != null && response.statusCode == 200) {
+				  var response = JSON.parse(data);
+				  idd = response.id;
+				  req.session.userid = idd;
+			  }
+			  res.redirect('/');
+		});
 };
 
 function handleReturnServer(data, response) {
