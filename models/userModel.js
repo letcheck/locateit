511e76676824000080d8a8fa20995120fd320cb5
Vincent L'honoré <vincent.lhonore@gmail.com>
2013-12-05 11:52:07 +0100

    some improvment of the user model

--- models/userModel.js
+++ models/userModel.js
@@ -7,7 +7,8 @@ var userSchema = new Schema({
 userid: String,
 email: String,
 name: String,
-from: {type: String, default: "google"}
+from: {type: String, default: "google"},
+sendMail: Boolean
 });
  
 module.exports = mongoose.model('User', userSchema);
--- routes/user.js
+++ routes/user.js
@@ -29,12 +29,12 @@ exports.add = function(req, res){
 	
 	//console.log(req.body.name);
 	
-	var id = createId(req.body.name);//req.body.email
+	var id = createId(req.body.email);//req.body.email
 	var user = exports.findById(id, function(user){
 		if(user == null)
 		{
 			//insert new user
-			new User({userid: id, email: "no@email.com", name: req.body.name}).save();
+			new User({userid: id, email: req.body.email, name: req.body.name, sendMail: req.body.sendMail}).save();
 		}
 		res.send('{"status" : "ok", "id" : "'+id+'"}');
 	});
@@ -54,7 +54,7 @@ exports.findById = function (id, fct){
 	query.exec(function (err, res) {
 		  if (err) { throw err; }
 		  else
-		  {console.log(res.length);console.log(res[0]);
+		  {//console.log(res.length);console.log(res[0]);
 			  if(res.length > 0)
 				  fct( res[0]);
 			  else
