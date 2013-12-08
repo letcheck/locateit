f66ad8fd5b0796df682e27a6e15f97eb5ec40528
Vincent L'honoré <vincent.lhonore@gmail.com>
2013-12-08 14:55:56 +0100

    fix double marker on post

--- public/js/post.js
+++ public/js/post.js
@@ -86,10 +86,11 @@ function codeAddress() {
 	  geocoder.geocode( { 'address': address}, function(results, status) {
 	    if (status == google.maps.GeocoderStatus.OK) {
 	      map.setCenter(results[0].geometry.location);
-	      marker = new google.maps.Marker({
+	      addMarker(results[0].geometry.location);
+	      /*marker = new google.maps.Marker({
 	          map: map,
 	          position: results[0].geometry.location
-	      });
+	      });*/
 
 	    } else {
 	      alert('Geocode was not successful for the following reason: ' + status);
